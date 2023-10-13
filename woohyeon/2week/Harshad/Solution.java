16b1fb5cc740f57d3a568265d3326b06b0c5c4e8
rain_hyeon <iiilee0907@naver.com>
2023-10-13 10:44:52 +0900

    2주차 수정

--- woohyeon/2week/Harshad/Solution.java
+++ woohyeon/2week/Harshad/Solution.java
@@ -24,4 +24,26 @@ class Solution {
 
         return answer;
     }
+
+    //수정내용
+     public boolean solution2(int x) {
+        boolean answer = true;
+        
+        int sum = 0;
+        int n = x;
+        
+         //10의자리 ~ 10000의자리의 합
+        for(int i=0; i<4; i++){
+            System.out.println(x%10);
+            sum += x%10; //나머지 값이 자리수
+            x /= 10; //몫은 다시 x로 가짐
+        }
+        
+        if(n % sum == 0) //나누어 떨어진다면
+            answer = true;
+        else
+            answer = false;
+        
+        return answer;
+    }
 }
