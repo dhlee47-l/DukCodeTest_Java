ed3b3e134abf7bc22c1ac4724938f7b978259d85
seoyeong <qlqltjdud@gmail.com>
2023-10-20 04:53:38 +0900

    Week03

---
+++ KSY/Week03/Print/solution.java
@@ -0,0 +1,13 @@
+package KSY.Week03.Print;
+import java.util.Scanner;
+
+public class solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.print(a);
+        sc.close();
+    }
+}
+
+// scanner close() 하기 
